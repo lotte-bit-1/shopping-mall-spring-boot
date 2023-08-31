4d23d034d94fc2c4a7e51212cb74b40c7f98661c
nowgnas <dev.nowgnas@gmail.com>
2023-08-31 14:14:43 +0900

    add: add category mapper interface

--- src/main/java/com/bit/shoppingmall/app/mapper/CategoryMapper.java
+++ src/main/java/com/bit/shoppingmall/app/mapper/CategoryMapper.java
@@ -1,8 +1,31 @@
 package com.bit.shoppingmall.app.mapper;
 
+import com.bit.shoppingmall.app.dto.category.response.CategoryHierarchy;
+import com.bit.shoppingmall.app.dto.category.response.SubCategory;
+import com.bit.shoppingmall.app.dto.product.ProductListItem;
+import com.bit.shoppingmall.app.entity.Category;
+import java.util.List;
+import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CategoryMapper {
+
+    Category select(Long id);
+
+    List<CategoryHierarchy> selectAll();
+
+    int insert(Category category);
+
+    int update(Category category);
+
+    int delete(Long id);
+
+    List<Category> firstCategory();
+
+    List<ProductListItem> searchProductByCategory(Map<String, Object> map);
+
+    List<SubCategory> selectSubcategory(String keyword);
+
 
 }
