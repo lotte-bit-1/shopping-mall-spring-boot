3b8b26f83c67f28b9be8cdd1dc5575a85eaef8f7
nowgnas <dev.nowgnas@gmail.com>
2023-08-31 14:34:48 +0900

    add: add category image mapper

--- src/main/java/com/bit/shoppingmall/app/mapper/ProductImageMapper.java
+++ src/main/java/com/bit/shoppingmall/app/mapper/ProductImageMapper.java
@@ -1,8 +1,14 @@
 package com.bit.shoppingmall.app.mapper;
 
+import com.bit.shoppingmall.app.entity.ProductImage;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ProductImageMapper {
-
+int delete(Long id);
+  int insert(ProductImage image);
+  int update(ProductImage image);
+  ProductImage select(Long id);
+  List<ProductImage> selectAll();
 }
