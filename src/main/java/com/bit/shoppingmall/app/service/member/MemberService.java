31ac45f9863021b3a1a5b2489b543edda4e60daa
JunYong Sun <ssjy4974@naver.com>
2023-08-31 12:43:30 +0900

    add : register feature

--- src/main/java/com/bit/shoppingmall/app/service/member/MemberService.java
+++ src/main/java/com/bit/shoppingmall/app/service/member/MemberService.java
@@ -1,6 +1,8 @@
 package com.bit.shoppingmall.app.service.member;
 
 import com.bit.shoppingmall.app.dto.member.request.MemberRegisterDto;
+import com.bit.shoppingmall.app.entity.Encryption;
+import com.bit.shoppingmall.app.entity.Member;
 import com.bit.shoppingmall.app.mapper.EncryptionMapper;
 import com.bit.shoppingmall.app.mapper.MemberMapper;
 import com.bit.shoppingmall.app.utils.CipherUtil;
@@ -17,6 +19,19 @@ public class MemberService {
 
     private final MemberMapper memberMapper;
     private final EncryptionMapper encryptionMapper;
+
+    public Boolean register(MemberRegisterDto dto) throws Exception {
+        String salt = createSalt();
+        String hashedPassword = createHashedPassword(dto.getPassword(), salt);
+
+        Member member = dto.toEntity(hashedPassword);
+        memberMapper.insert(member);
+
+        Encryption encryption = Encryption.from(member, salt);
+        int result = encryptionMapper.insert(encryption);
+
+        return result == 1 ? true : false;
+    }
 
     private String createHashedPassword(String password, String salt) throws Exception {
         new String();
