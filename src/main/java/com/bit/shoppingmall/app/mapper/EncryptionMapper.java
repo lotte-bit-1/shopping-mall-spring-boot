ba42c01360df3432dc4d9dd231e4ec288fc3f895
JunYong Sun <ssjy4974@naver.com>
2023-08-31 12:22:45 +0900

    add : EncryptionMapper 추가

--- src/main/java/com/bit/shoppingmall/app/mapper/EncryptionMapper.java
+++ src/main/java/com/bit/shoppingmall/app/mapper/EncryptionMapper.java
@@ -1,8 +1,25 @@
 package com.bit.shoppingmall.app.mapper;
 
+import com.bit.shoppingmall.app.entity.Encryption;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface EncryptionMapper {
+
+    Optional<Encryption> select(Long id);
+
+    Optional<Encryption> selectByEmail(String email);
+
+    List<Encryption> selectall();
+
+    void insert(Encryption encryption);
+
+    void update(Encryption encryption);
+
+    void delete(Long id);
+
 
 }
