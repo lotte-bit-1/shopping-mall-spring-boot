c7a73689e0412632c8f988686c8f67ff0f3ac24d
JunYong Sun <ssjy4974@naver.com>
2023-09-01 09:55:14 +0900

    fix : register 성공 status 수정

--- src/main/java/com/bit/shoppingmall/web/restcontroller/MemberRestController.java
+++ src/main/java/com/bit/shoppingmall/web/restcontroller/MemberRestController.java
@@ -1,25 +1,28 @@
 package com.bit.shoppingmall.web.restcontroller;
 
+import com.bit.shoppingmall.app.dto.member.request.LoginDto;
 import com.bit.shoppingmall.app.dto.member.request.MemberRegisterDto;
 import com.bit.shoppingmall.app.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/memberApi/*")
+@RequestMapping("/memberApi")
 public class MemberRestController {
 
     private final MemberService memberService;
 
     @PostMapping("/register")
     public ResponseEntity<Boolean> memberAdd(@RequestBody MemberRegisterDto dto) throws Exception {
-        return ResponseEntity.ok(memberService.addMember(dto));
+        return new ResponseEntity<>(memberService.addMember(dto), HttpStatus.CREATED);
     }
 
     @GetMapping("/loginCheck/{email}")
     public ResponseEntity<Boolean> loginCheck(@PathVariable("email") String email) throws Exception {
         return ResponseEntity.ok(memberService.isDuplicatedEmail(email));
     }
+    
 }
