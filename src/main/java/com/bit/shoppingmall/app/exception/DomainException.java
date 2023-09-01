2f419a107717969e6ff1a7ab0c4e67394e205c8b
JunYong Sun <ssjy4974@naver.com>
2023-09-01 20:10:42 +0900

    add : exceptionhandler 추가

--- src/main/java/com/bit/shoppingmall/app/exception/DomainException.java
+++ src/main/java/com/bit/shoppingmall/app/exception/DomainException.java
@@ -1,10 +1,21 @@
 package com.bit.shoppingmall.app.exception;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
 public abstract class DomainException extends RuntimeException {
+    private final Map<String, String> validation = new HashMap<>();
 
-  public DomainException(String message) {
-    super(message);
-  }
+    public DomainException(String message) {
+        super(message);
+    }
 
-  public abstract int getStatusCode();
+    public abstract int getStatusCode();
+
+    public void addValidation(String fieldName, String errorMessage) {
+        validation.put(fieldName, errorMessage);
+    }
 }
