1c9e1dc318463ffd1462df6af1ac18d6655e30c2
MJun111 <kmj0568@gmail.com>
2023-09-01 12:16:37 +0900

    test: add likes test

---
+++ src/main/java/com/bit/shoppingmall/app/service/likes/ProductLikesService.java
@@ -0,0 +1,106 @@
+package com.bit.shoppingmall.app.service.likes;
+
+import com.bit.shoppingmall.app.dto.likes.request.LikesSelectForPage;
+import com.bit.shoppingmall.app.dto.likes.response.LikesListWithPagination;
+import com.bit.shoppingmall.app.dto.paging.Pagination;
+import com.bit.shoppingmall.app.dto.product.ProductListItemOfLike;
+import com.bit.shoppingmall.app.entity.Likes;
+import com.bit.shoppingmall.app.entity.ProductAndMemberCompositeKey;
+import com.bit.shoppingmall.app.exception.likes.LikesEntityDuplicateException;
+import com.bit.shoppingmall.app.exception.likes.LikesEntityNotFoundException;
+import com.bit.shoppingmall.app.mapper.LikesMapper;
+import com.bit.shoppingmall.app.mapper.ProductMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ProductLikesService {
+
+  private final LikesMapper likesMapper;
+  private final ProductMapper productMapper;
+
+  public LikesListWithPagination getMemberLikes(Long memberId, Integer curPage) throws Exception {
+    try {
+      int perPage = 5;
+      int totalPage = (int) Math.ceil((float) likesMapper.selectTotalCount(memberId) / perPage);
+
+      int start = (curPage - 1) * perPage;
+      LikesSelectForPage likesSelectForPage =
+          LikesSelectForPage.builder().memberId(memberId).start(start).PerPage(perPage).build();
+
+      List<Long> productIdList = likesMapper.selectall(likesSelectForPage);
+      if (productIdList.isEmpty()) {
+        throw new LikesEntityNotFoundException();
+      }
+
+      List<ProductListItemOfLike> memberLikesList =
+          productMapper.selectProductListItemOfLike(productIdList);
+
+      Pagination paging =
+          Pagination.builder()
+              .currentPage(Math.toIntExact(curPage))
+              .totalPage(totalPage)
+              .perPage(perPage)
+              .build();
+
+      return LikesListWithPagination.builder().list(memberLikesList).paging(paging).build();
+    } catch (LikesEntityNotFoundException e) {
+      e.printStackTrace();
+      return LikesListWithPagination.builder().build();
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new Exception(e.getMessage());
+    }
+  }
+
+  public int addLikes(ProductAndMemberCompositeKey productAndMemberCompositeKey) {
+    int res = 0;
+    try {
+      res =
+          likesMapper.insert(
+              new Likes(
+                  productAndMemberCompositeKey.getMemberId(),
+                  productAndMemberCompositeKey.getProductId())
+          );
+    } catch (DuplicateKeyException e) {
+      e.printStackTrace();
+      throw new LikesEntityDuplicateException();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return res;
+  }
+
+  public int removeLikes(ProductAndMemberCompositeKey productAndMemberCompositeKey) {
+    int res = 0;
+    try {
+      res = likesMapper.delete(productAndMemberCompositeKey);
+      if (res == 0) {
+        throw new LikesEntityNotFoundException();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return res;
+  }
+
+  public int removeSomeLikes(List<ProductAndMemberCompositeKey> keyList) {
+    int res = 0;
+    try {
+      for (ProductAndMemberCompositeKey key : keyList) {
+        res = likesMapper.delete(key) > 0 ? res + 1 : res;
+      }
+      if (res == 0) {
+        throw new LikesEntityNotFoundException();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return res;
+  }
+}
--- src/test/java/com/bit/shoppingmall/app/mapper/likes/LikesMapperTest.java
+++ src/test/java/com/bit/shoppingmall/app/mapper/likes/LikesMapperTest.java
@@ -22,8 +22,10 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.transaction.annotation.Transactional;
 
 @MybatisTest
+@Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class LikesMapperTest {
 
@@ -36,21 +38,21 @@ public class LikesMapperTest {
   @Autowired
   CategoryMapper categoryMapper;
 
-  Likes likes = Likes.builder().memberId(1L).productId(1L).build();
-  ProductAndMemberCompositeKey compKey = ProductAndMemberCompositeKey.builder().memberId(1L)
-      .productId(1L).build();
+  Member member;
+  Product product;
+  Product product2;
 
   @BeforeEach
   void before() throws Exception {
-    Member member = Member.builder().email("test@naver.com").password("test123").name("상셀")
+    member = Member.builder().email("test@naver.com").password("test123").name("상셀")
         .money(1000000L).build();
     memberMapper.insert(member);
     Category category = Category.builder().name("카테고리1").level(1).build();
     categoryMapper.insert(category);
-    Product product = Product.builder().name("상품").description("설명").price(10000L).quantity(100L)
-        .code("PRODUCT-001").categoryId(1L).build();
-    Product product2 = Product.builder().name("상품2").description("설명2").price(10000L).quantity(100L)
-        .code("PRODUCT-002").categoryId(1L).build();
+    product = Product.builder().name("상품").description("설명").price(10000L).quantity(100L)
+        .code("PRODUCT-001").categoryId(category.getId()).build();
+    product2 = Product.builder().name("상품2").description("설명2").price(10000L).quantity(100L)
+        .code("PRODUCT-002").categoryId(category.getId()).build();
     productMapper.insert(product);
     productMapper.insert(product2);
   }
@@ -58,6 +60,7 @@ public class LikesMapperTest {
   @DisplayName("찜 추가 테스트")
   @Test
   public void insert() throws Exception {
+    Likes likes = Likes.builder().memberId(member.getId()).productId(product.getId()).build();
     int res = likesMapper.insert(likes);
 
     assertTrue(res == 1);
@@ -66,6 +69,8 @@ public class LikesMapperTest {
   @DisplayName("찜 중복 추가 테스트")
   @Test
   public void insertDuplicate() throws Exception {
+    Likes likes = Likes.builder().memberId(member.getId()).productId(product.getId()).build();
+
     assertThrows(DuplicateKeyException.class, () -> {
       int res1 = likesMapper.insert(likes);
       int res2 = likesMapper.insert(likes);
@@ -75,6 +80,10 @@ public class LikesMapperTest {
   @DisplayName("찜 삭제 테스트")
   @Test
   public void delete() throws Exception {
+    Likes likes = Likes.builder().memberId(member.getId()).productId(product.getId()).build();
+    ProductAndMemberCompositeKey compKey = ProductAndMemberCompositeKey.builder()
+        .memberId(member.getId()).productId(product.getId()).build();
+
     int insertRes = likesMapper.insert(likes);
     int deleteRes = likesMapper.delete(compKey);
 
@@ -84,6 +93,10 @@ public class LikesMapperTest {
   @DisplayName("찜 중복 삭제 테스트")
   @Test
   public void deleteDuplicate() throws Exception {
+    Likes likes = Likes.builder().memberId(member.getId()).productId(product.getId()).build();
+    ProductAndMemberCompositeKey compKey = ProductAndMemberCompositeKey.builder()
+        .memberId(member.getId()).productId(product.getId()).build();
+
     int insertRes = likesMapper.insert(likes);
     int deleteRes = likesMapper.delete(
         compKey);
@@ -97,6 +110,10 @@ public class LikesMapperTest {
   @DisplayName("찜 조회 테스트")
   @Test
   public void select() throws Exception {
+    Likes likes = Likes.builder().memberId(member.getId()).productId(product.getId()).build();
+    ProductAndMemberCompositeKey compKey = ProductAndMemberCompositeKey.builder()
+        .memberId(member.getId()).productId(product.getId()).build();
+
     int inputRes = likesMapper.insert(likes);
     Likes selectLikes = likesMapper.select(compKey).get();
 
@@ -107,13 +124,16 @@ public class LikesMapperTest {
   @DisplayName("회원 찜 목록 조회")
   @Test
   public void selectAll() throws Exception {
+    Likes likes = Likes.builder().memberId(member.getId()).productId(product.getId()).build();
+    Likes likes2 = Likes.builder().memberId(member.getId()).productId(product2.getId()).build();
+
     likesMapper.insert(likes);
-    likesMapper.insert(Likes.builder().productId(2L).memberId(1L).build());
+    likesMapper.insert(likes2);
 
     List<Long> list = likesMapper.selectall(
-        LikesSelectForPage.builder().memberId(1L).start(0).PerPage(5).build());
+        LikesSelectForPage.builder().memberId(member.getId()).start(0).PerPage(5).build());
 
-    long idx = 1L;
+    long idx = product.getId();
     for (Long productId : list) {
       assertEquals(productId, idx++);
     }
