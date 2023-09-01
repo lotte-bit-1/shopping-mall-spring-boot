4dbad883f4ddb104eda19d690a493a79872a3c39
UNG <rnwldnd7248@gmail.com>
2023-09-01 15:21:49 +0900

    remove: remove empty cart test

--- src/main/java/com/bit/shoppingmall/web/interceptor/LoginInterceptor.java
+++ src/main/java/com/bit/shoppingmall/web/interceptor/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package com.bit.shoppingmall.web.interceptor;
 
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -7,11 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        log.info("Interceptor URI: "+ request.getRequestURI());
         HttpSession session = request.getSession(false);
         if(session == null || session.getAttribute("loginMember") == null) {
             response.sendRedirect("/");
