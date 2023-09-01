c25923aeea62f7c19e1b152689a40655bf5bb0fa
JunYong Sun <ssjy4974@naver.com>
2023-09-01 17:11:28 +0900

    add : logging, executiontime aspect 추가

--- src/main/java/com/bit/shoppingmall/web/controller/MainController.java
+++ src/main/java/com/bit/shoppingmall/web/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.bit.shoppingmall.web.controller;
 
-import com.bit.shoppingmall.app.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
