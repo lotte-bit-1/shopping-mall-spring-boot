a3d932b40b94faa0f84021257bd9749c3241dff7
CokeLee777 <akak640@kyonggi.ac.kr>
2023-09-03 13:40:33 +0900

    feat: add order and kakao payment logic

--- src/main/java/com/bit/shoppingmall/web/restcontroller/OrderRestController.java
+++ src/main/java/com/bit/shoppingmall/web/restcontroller/OrderRestController.java
@@ -6,8 +6,10 @@ import com.bit.shoppingmall.app.dto.order.request.OrderCreateDto;
 import com.bit.shoppingmall.app.entity.Order;
 import com.bit.shoppingmall.app.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @RestController
@@ -23,14 +25,18 @@ public class OrderRestController {
     }
 
     @PostMapping("/direct")
-    public Long createOrder(@ModelAttribute("memberId") Long memberId, @Valid @RequestBody OrderCreateDto orderCreateDto) throws Exception {
+    public ResponseEntity<Long> createOrder(
+            @ModelAttribute("memberId") Long memberId,
+            @Valid @RequestBody OrderCreateDto orderCreateDto) throws Exception {
         orderCreateDto.setMemberId(memberId);
         Order order = orderService.createOrder(orderCreateDto);
-        return order.getId();
+
+        return ResponseEntity.ok().body(order.getId());
     }
 
     @PostMapping("/cart")
-    public Long createCartOrder(@ModelAttribute("memberId") Long memberId, @Valid @RequestBody OrderCartCreateDto orderCartCreateDto) throws Exception {
+    public Long createCartOrder(@ModelAttribute("memberId") Long memberId,
+                                @Valid @RequestBody OrderCartCreateDto orderCartCreateDto) throws Exception {
         orderCartCreateDto.setMemberId(memberId);
         Order cartOrder = orderService.createCartOrder(orderCartCreateDto);
         return cartOrder.getId();
