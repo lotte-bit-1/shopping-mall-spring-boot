6bd0d681e9b77081b08aceeb742a59472e9bce94
nowgnas <dev.nowgnas@gmail.com>
2023-09-04 09:54:32 +0900

    fix: amend product keyword not match exception

--- src/main/java/com/bit/shoppingmall/web/advice/ProductControllerAdvice.java
+++ src/main/java/com/bit/shoppingmall/web/advice/ProductControllerAdvice.java
@@ -7,17 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Order(0)
 @ControllerAdvice(annotations = Controller.class)
 public class ProductControllerAdvice {
 
-    @ExceptionHandler(ProductKeywordNotMatchException.class)
-    public ModelAndView domainException(ProductKeywordNotMatchException ex, Model model) {
-        log.warn("WARN={}", ex.getMessage());
-        model.addAttribute("errorMessage", ex.getMessage());
-        return new ModelAndView("redirect:/product/1/list");
-    }
+  @ExceptionHandler(ProductKeywordNotMatchException.class)
+  public String domainException(ProductKeywordNotMatchException ex, Model model) {
+    log.warn("WARN={}", ex.getMessage());
+    model.addAttribute("error", ex.getMessage());
+    return "product/list";
+  }
 }
